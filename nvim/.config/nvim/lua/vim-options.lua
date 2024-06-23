d77d2891128360fdf68ac927815d0b9b3959bbca
Mohamed-elg <mohamed.elguermat9@gmail.com>
2024-06-23 18:45:57 +0200

    nvim

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set nu")
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap('n', '<C-Tab>', '<C-w>w', { noremap = true, silent = true })
+vim.opt.guifont = "MesloLGM Nerd Font:h12"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -115,6 +115,7 @@ alias k="kubectl"
 alias h="helm"
 alias tohypr="nvim ~/.config/hypr/hyprland.conf"
 alias p="pikaur"
+alias tocode="cd ~/Bureau/Code"
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
