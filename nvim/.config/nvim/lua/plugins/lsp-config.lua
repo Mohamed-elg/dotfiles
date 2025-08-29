2cefe849eef1aee420467b493769c30fbe790a82
Mohamed E <105205147+Mohamed-elg@users.noreply.github.com>
2025-08-29 14:37:50 +0200

    Update lsp-config.lua

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -8,51 +8,22 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
-		-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		lspconfig.terraformls.setup({ capabilities = capabilities })
-		lspconfig.ts_ls.setup({ capabilities = capabilities })
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-			filetypes = { "python" },
-			settings = {
-				python = {
-					analysis = {
-						typeCheckingMode = "strict",
-						diagnosticSeverityOverrides = {
-							reportAttributeAccessIssue = "none",
-							reportUnknownMemberType = "none",
-						},
-					},
-				},
-			},
-		})
-		lspconfig.html.setup({ capabilities = capabilities })
-		lspconfig.yamlls.setup({ capabilities = capabilities })
-		lspconfig.gopls.setup({ capabilities = capabilities })
-		lspconfig.vuels.setup({ capabilities = capabilities })
-		lspconfig.angularls.setup({ capabilities = capabilities })
-		lspconfig.vuels.setup({ capabilities = capabilities })
-		lspconfig.dockerls.setup({ capabilities = capabilities })
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-					completion = {
-						callSnippet = "Replace",
-					},
-				},
-			},
-		})
+		vim.lsp.enable("terraformls")
+		vim.lsp.enable("ts_ls")
+		vim.lsp.enable("pyright")
+		vim.lsp.enable("html")
+		vim.lsp.enable("yamlls")
+		vim.lsp.enable("gopls")
+		vim.lsp.enable("vuels")
+		vim.lsp.enable("angularls")
+		vim.lsp.enable("vuels")
+		vim.lsp.enable("dockerls")
+		vim.lsp.enable("lua_ls")
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Details" })
 		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Definition" })
 		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Reference" })
 		vim.keymap.set("n", "<leader>gt", "<cmd>Telescope lsp_references<CR>", { desc = "Telescope reference" })
-		-- vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, { desc = "Code action" })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { desc = "Rename a variable" })
 	end,
 }
